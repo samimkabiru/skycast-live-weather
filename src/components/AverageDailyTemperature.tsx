7e1200d8ea21ac4f07b1139296f0a99e4bfec2d9
Samim-kabiru <samimkabiru@gmail.com>
2025-09-06 17:47:19 +0100

    Show progressive dates in the average daily temperature component.

--- src/components/AverageDailyTemperature.tsx
+++ src/components/AverageDailyTemperature.tsx
@@ -18,7 +18,7 @@ interface FetchForecastDaysResponse {
     name: string;
   };
   forecast: {
-    forecastday: { day: { avgtemp_c: number } }[];
+    forecastday: { date: string; day: { avgtemp_c: number } }[];
   };
 }
 
@@ -37,46 +37,13 @@ const AverageDailyTemperature = () => {
         .then((res) => res.data),
   });
   const forecast = dailyForecast?.forecast.forecastday;
-  const forecastMap = {
-    day1: forecast?.[0].day,
-    day2: forecast?.[1].day,
-    day3: forecast?.[2].day,
-    day4: forecast?.[3].day,
-    day5: forecast?.[4].day,
-    day6: forecast?.[5].day,
-    day7: forecast?.[6].day,
-  };
-
-  const data = [
-    {
-      name: 'Day 1',
-      temp: forecastMap.day1?.avgtemp_c,
-    },
-    {
-      name: 'Day 2',
-      temp: forecastMap.day2?.avgtemp_c,
-    },
-    {
-      name: 'Day 3',
-      temp: forecastMap.day3?.avgtemp_c,
-    },
-    {
-      name: 'Day 4',
-      temp: forecastMap.day4?.avgtemp_c,
-    },
-    {
-      name: 'Day 5',
-      temp: forecastMap.day5?.avgtemp_c,
-    },
-    {
-      name: 'Day 6',
-      temp: forecastMap.day6?.avgtemp_c,
-    },
-    {
-      name: 'Day 7',
-      temp: forecastMap.day7?.avgtemp_c,
-    },
-  ];
+  const data = forecast?.map((forecast) => ({
+    name: new Date(forecast.date).toLocaleDateString(navigator.language, {
+      month: 'short',
+      day: 'numeric',
+    }),
+    temp: forecast.day.avgtemp_c,
+  }));
 
   return (
     <Card>
